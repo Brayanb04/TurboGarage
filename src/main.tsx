cf41e7e48c93492bc8adf2c0a7209aef652ed624
Brayanb04 <brayan.bautista.0428@gmail.com>
2025-10-29 21:45:04 -0500

    Guardando cambios locales antes del pull

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HotWheelsCollector from './hotwheels-collector'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HotWheelsCollector />
+  </React.StrictMode>,
+)
