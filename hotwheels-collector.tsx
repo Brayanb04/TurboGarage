37a91326aa9efaed92cd172f356db83a587e2d97
Brayanb04 <brayan.bautista.0428@gmail.com>
2025-10-30 18:40:17 -0500

    Descripción de los cambios en hotwheels-collector.tsx

--- hotwheels-collector.tsx
+++ hotwheels-collector.tsx
@@ -432,7 +432,7 @@ export default function HotWheelsCollector() {
         )}
       </div>
 
-      {/* MODAL AGREGAR / EDITAR */}
+      {/* MODAL AGREGAR y EDITAR */}
       {showAddModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4 overflow-y-auto">
           <div className="bg-blue-950 rounded-xl max-w-md w-full my-8 border border-gray-600">
